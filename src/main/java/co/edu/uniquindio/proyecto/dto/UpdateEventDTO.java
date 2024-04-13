72079b97c5a108011403431aef39341b2f4e96e8
Daniela Sofia Alzate Franco <danielas.alzatef@uqvirtual.edu.co>
2024-04-13 14:13:50 -0500

    DT0: Documentar mensaje de error en UpdateEventDTO

--- src/main/java/co/edu/uniquindio/proyecto/dto/UpdateEventDTO.java
+++ src/main/java/co/edu/uniquindio/proyecto/dto/UpdateEventDTO.java
@@ -1,13 +1,15 @@
 package co.edu.uniquindio.proyecto.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 import java.time.LocalDateTime;
 
 public record UpdateEventDTO(
-        String id,
-        String description,
-        LocalDateTime date,
-        String title,
-        String client,
-        String business
+        @NotBlank(message = "Es necesario que ingrese el id") String id,
+        @NotBlank(message = "Es necesario que ingrese la descripcion") String description,
+        @NotBlank(message = "Es necesario que ingrese el horario local") LocalDateTime date,
+        @NotBlank(message = "Es necesario que ingrese un titulo") String title,
+        @NotBlank(message = "Es necesario que ingrese el cliente") String client,
+        @NotBlank(message = "Es necesario que ingrese el negocio") String business
 ) {
 }
