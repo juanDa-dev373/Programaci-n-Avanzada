e7b1f94122507ae7a6bd011813e5880e4730e597
Daniela Sofia Alzate Franco <danielas.alzatef@uqvirtual.edu.co>
2024-04-13 13:27:31 -0500

    DT0: Documentar mensaje de error en CalificationDTO

--- src/main/java/co/edu/uniquindio/proyecto/dto/CalificationDTO.java
+++ src/main/java/co/edu/uniquindio/proyecto/dto/CalificationDTO.java
@@ -1,9 +1,11 @@
 package co.edu.uniquindio.proyecto.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record CalificationDTO(
-        String id,
-        String idCliente,
-        String idBusiness,
-        int calification
+        @NotBlank(message = "Es necesario que ingrese el id") String id,
+        @NotBlank(message = "Es necesario que ingrese el id del cliente") String idCliente,
+        @NotBlank(message = "Es necesario que ingrese el id de Business") String idBusiness,
+        @NotBlank(message = "Es necesario que ingrese la calificacion") int calification
 ) {
 }
