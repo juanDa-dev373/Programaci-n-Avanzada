7d2186ed6f7d807f940fff12bf64a846dfd7a12a
Daniela Sofia Alzate Franco <danielas.alzatef@uqvirtual.edu.co>
2024-04-13 13:17:45 -0500

    DT0: Documentar mensaje de error

--- src/main/java/co/edu/uniquindio/proyecto/dto/AddBusinessDTO.java
+++ src/main/java/co/edu/uniquindio/proyecto/dto/AddBusinessDTO.java
@@ -17,7 +17,7 @@ public record AddBusinessDTO(
         @NotBlank String id,
         @NotBlank @Min(1) @Max(50) String name,
         @NotBlank @Max(400) @Min(20) String description,
-        @NotBlank String idClient,
+        @NotBlank(message = "Es necesario que ingrese el id del cliente ") String idClient,
         @NotBlank Location location,
         @NotBlank List<String>images,
         @NotBlank TypeBusiness typeBusiness,
