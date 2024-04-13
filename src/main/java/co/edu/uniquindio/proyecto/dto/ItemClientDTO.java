7bd4b1f72998809e1a45ab3136f368845650085f
Daniela Sofia Alzate Franco <danielas.alzatef@uqvirtual.edu.co>
2024-04-13 13:41:13 -0500

    DT0: Documentar mensaje de error en ItemClientDTO

--- src/main/java/co/edu/uniquindio/proyecto/dto/ItemClientDTO.java
+++ src/main/java/co/edu/uniquindio/proyecto/dto/ItemClientDTO.java
@@ -3,9 +3,9 @@ package co.edu.uniquindio.proyecto.dto;
 import jakarta.validation.constraints.NotBlank;
 
 public record ItemClientDTO(
-        @NotBlank String id,
-        @NotBlank String name,
-        @NotBlank String profilePhoto,
-        @NotBlank String nickname,
-        @NotBlank String city) {
+        @NotBlank(message = "Es necesario que ingrese el id") String id,
+        @NotBlank(message = "Es necesario que ingrese el nombre") String name,
+        @NotBlank(message = "Es necesario que ingrese la foto de perfil") String profilePhoto,
+        @NotBlank(message = "Es necesario que ingrese el nickname") String nickname,
+        @NotBlank(message = "Es necesario que ingrese la ciudad") String city) {
 }
